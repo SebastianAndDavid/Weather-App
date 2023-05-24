afb1f8254c2db02806044d05daf01919abc02c2c
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-05-24 11:26:13 -0700

    ft. cleared inputs after login/signup

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { userSignUp } from "../Services/supabase-utils";
+import { userSignIn, userSignUp } from "../Services/supabase-utils";
 
 export default function Auth() {
   const [email, setEmail] = useState("");
@@ -8,17 +8,29 @@ export default function Auth() {
   async function handleSignUp(e) {
     e.preventDefault();
     const res = await userSignUp(email, password);
+    setEmail("");
+    setPassword("");
+    return res;
+  }
+
+  async function handleSignIn(e) {
+    e.preventDefault();
+    const res = await userSignIn(email, password);
+    setEmail("");
+    setPassword("");
     return res;
   }
 
   return (
     <div>
+      Sign Up
       <form onSubmit={(e) => handleSignUp(e)}>
         <label>
           Email
           <input
             type="email"
             placeholder="email"
+            value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </label>
@@ -27,6 +39,30 @@ export default function Auth() {
           <input
             type="password"
             placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+      <br></br>
+      Sign In
+      <form onSubmit={(e) => handleSignIn(e)}>
+        <label>
+          Email
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
