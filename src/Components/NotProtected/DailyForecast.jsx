e504da9c655a283b9aac131b49ea87fcf618190c
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-06-01 12:04:28 -0700

    ft. added homepage with api call

---
+++ src/Components/NotProtected/DailyForecast.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+
+export default function DailyForecast({ cityWeather }) {
+  const cityName = cityWeather.location.name;
+  const cityCountry = cityWeather.location.country;
+  const tempF = cityWeather.current.temp_f;
+  const feelsLike = cityWeather.current.feelslike_f;
+
+  return (
+    <div className="daily-forecast">
+      <h4>{cityName}</h4>
+      <p>Country: {cityCountry}</p>
+      <p>Current Temperature: {tempF}</p>
+      {cityWeather.current.temp_f != feelsLike && (
+        <p>Feels like: {feelsLike}</p>
+      )}
+    </div>
+  );
+}
--- src/Components/NotProtected/Home.jsx
+++ src/Components/NotProtected/Home.jsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+import { fetchWeather } from "../../Utils/weather-utils";
+import DailyForecast from "./DailyForecast";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [searchCity, setSearchCity] = useState("");
+  const [cityWeather, setCityWeather] = useState({});
+
+  async function handleFetchWeather() {
+    const data = await fetchWeather(searchCity);
+    if (data === "error") {
+      return alert("Error: Please enter a valid location");
+    } else {
+      setCityWeather(data);
+      return data;
+    }
+  }
+
+  return (
+    <>
+      <div className="home">
+        <header className="search-field">
+          <h3>Weather App</h3>
+          <input
+            onChange={(e) => setSearchCity(e.target.value)}
+            placeholder="Search a City..."
+          />
+          <button onClick={handleFetchWeather}>Submit</button>
+          {/* <button onClick={() => handlelogOut()}>Logout</button> */}
+        </header>
+        <DailyForecast cityWeather={cityWeather} />
+      </div>
+    </>
+  );
 }
