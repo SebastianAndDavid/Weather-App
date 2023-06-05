355baab2c15123705fab052f17fe0001c35d779b
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-06-05 10:23:13 -0700

    ft. fixed useEffect depency issues, data now appearing on component mount and on click

--- src/Components/Protected/UserHome.jsx
+++ src/Components/Protected/UserHome.jsx
@@ -35,7 +35,7 @@ export default function UserHome() {
 
   useEffect(() => {
     handleFetchLastFiveCities();
-  }, [cityWeather]);
+  }, [user, cityWeather]);
 
   async function handlelogOut() {
     await logOut();
