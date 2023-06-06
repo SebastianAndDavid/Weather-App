dcc82e06023fc82f0edcb227c5448b7a0c964809
David <Rigby335@hotmail.com>
2023-06-06 12:04:08 -0700

    ft. added api calls to RecentCities to display dynamic background img

--- src/Components/Protected/RecentCities.jsx
+++ src/Components/Protected/RecentCities.jsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import { deleteCityById } from "../../Utils/supabase-utils";
+import { fetchWeather } from "../../Utils/weather-utils";
 
 /* eslint-disable react/prop-types */
 export default function RecentCities({ city }) {
+  const [cityObj, setCityObj] = useState({});
+
+  const cityCondition = cityObj?.current?.condition?.text;
+
+  const imageUrl = `../../../public/${cityCondition}.jpg`;
+  const inlineStyle = {
+    backgroundImage: `url(${imageUrl.replace(/\s/g, "%20")})`,
+  };
+  console.log("cityObj", cityObj);
+
+  async function handleFetchCityObj() {
+    const data = await fetchWeather(city.city);
+    console.log("data", data);
+    setCityObj(data);
+    return data;
+  }
+
+  useEffect(() => {
+    handleFetchCityObj();
+  }, []);
+
   async function handleCityDelete() {
     await deleteCityById(city.id);
   }
   return (
-    <div className="city-card">
+    <div className="city-card" style={inlineStyle}>
       <h4>{city.city}</h4>
       <label>
         Delete
